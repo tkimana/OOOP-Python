9485c5dcbb9999fe11a3a3d8c53d2f586dc8ec50
tkimana <kimana.theodore@gmail.com>
2021-02-11 13:46:51 -0600

    changes

--- customer.py
+++ customer.py
@@ -6,11 +6,45 @@ class Customer:
     def update_membership(self, new_membership_type):
         self.new_membership_type=new_membership_type
 
+
+    # Create a property
+    @property
+    def name(self):
+        return self._name
+
+
+    # Setter
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+   # To delete
+    @name.deleter
+    def name(self):
+        del self._name
+
     def __str__(self):
         print('Print String')
+        return self.name + " " + self.membership_type
+    
+    def print_all_customers(customers):
+        for customer in c:
+            print(customer)
+
+
+# Check if the atributes are similar 
+    def __eq__(self, other):
+        if self.name == other.name and self.membership_type == other.membership_type:
+
+          return True
+
+        return False
+
 
 c= [Customer('James', 'bronze'), Customer('Greg', 'gold')]
-c[0].update_membership('Silva')
-print(c[0].new_membership_type)
+
+Customer.print_all_customers(c)
+
+print(c[0].name)
 
 
